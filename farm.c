91c4b62c24598639866f7c90d7e470825e06f1e3
Anatras02 <alessio.balderi@adraise.it>
2022-06-15 16:23:22 +0200

    Soppresso log errori getopt

--- farm.c
+++ farm.c
@@ -14,7 +14,8 @@ int main(int argc, char *argv[]) {
     int lunghezza_buffer = 8;
     int delay = 0;
 
-    while ((opt = getopt(argc, argv, "n:q:t:")) != -1) {
+    opterr = 0;
+    while ((opt = getopt(argc, argv, "n:q:t:")) != -1) { //Se lo stesso parametro viene passato più volte prende l'ultimo
         switch (opt) {
             case 'n':
                 numero_thread = atoi(optarg);
@@ -26,9 +27,10 @@ int main(int argc, char *argv[]) {
                 delay = atoi(optarg);
                 break;
             default:
-                continue;
+                continue; //Se vengono passati altri parametri li salta
         }
     }
+    opterr = 1;
 
     return 0;
 }
