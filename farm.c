cf99de1d238d5046facabe004651365eaff6f552
Anatras02 <alessio.balderi@adraise.it>
2022-06-15 16:17:45 +0200

    Parsing opzioni

--- farm.c
+++ farm.c
@@ -1,10 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  // controlla numero argomenti
-  if (argc < 2) {
-    printf("Uso: %s file [file ...] \n", argv[0]);
-    return 1;
-  }
+    // controlla numero argomenti
+    if (argc < 2) {
+        printf("Uso: %s file [file ...] \n", argv[0]);
+        return 1;
+    }
 
-  return 0;
+    int opt;
+    int numero_thread = 4;
+    int lunghezza_buffer = 8;
+    int delay = 0;
+
+    while ((opt = getopt(argc, argv, "n:q:t:")) != -1) {
+        switch (opt) {
+            case 'n':
+                numero_thread = atoi(optarg);
+                break;
+            case 'q':
+                lunghezza_buffer = atoi(optarg);
+                break;
+            case 't':
+                delay = atoi(optarg);
+                break;
+            default:
+                continue;
+        }
+    }
+
+    return 0;
 }
