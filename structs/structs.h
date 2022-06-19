997c085995fe8ebbc1b247c70ce049386644b216
Anatras02 <alessio.balderi@adraise.it>
2022-06-19 15:52:03 +0200

    File con strutture dati

---
+++ structs/structs.h
@@ -0,0 +1,22 @@
+#include "../xerrori/xerrori.h"
+
+typedef struct {
+    int *pindex;  // indice nel buffer
+    char **buffer;
+    int dimensione_buffer;
+    pthread_mutex_t *pmutex;
+    sem_t *sem_free_slots;
+    sem_t *sem_data_items;
+    char **nomi_file;
+    int numero_file;
+} pdati;
+
+typedef struct {
+    int *cindex;  // indice nel buffer
+    char **buffer;
+    int dimensione_buffer;
+    pthread_mutex_t *cmutex;
+    sem_t *sem_free_slots;
+    sem_t *sem_data_items;
+    char *default_file_directory;
+} cdati;
