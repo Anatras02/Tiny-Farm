7a1f6f550afc197de213d417ba539cdcd000904c
Anatras02 <alessio.balderi@adraise.it>
2022-06-28 16:15:14 +0200

    Aggiunte funzioni di scrittura e lettura da socket e altre funzioni di utilità

--- funzioni/funzioni.c
+++ funzioni/funzioni.c
@@ -26,3 +26,60 @@ char **get_nomi_file(int argc, char *argv[], int *n) {
 
     return file_names;
 }
+
+int get_numero_cifre(long n) {
+    int count = 0;
+    while (n != 0) {
+        n = n / 10;
+        count++;
+    }
+
+    return count;
+}
+
+char *genera_stringa_socket(char *filename, long sum, int *dimensione_out) {
+    int dimensione = get_numero_cifre(sum) + strlen(filename) + 1;
+    char *buffer = malloc(dimensione * sizeof(char));
+
+    *dimensione_out = dimensione;
+
+    sprintf(buffer, "%s|%ld", filename, sum);
+
+
+    return buffer;
+}
+
+/* Read "n" bytes from a descriptor */
+ssize_t readn(int fd, void *ptr, size_t n) {
+    size_t nleft;
+    ssize_t nread;
+
+    nleft = n;
+    while (nleft > 0) {
+        if ((nread = read(fd, ptr, nleft)) < 0) {
+            if (nleft == n) return -1; /* error, return -1 */
+            else break; /* error, return amount read so far */
+        } else if (nread == 0) break; /* EOF */
+        nleft -= nread;
+        ptr += nread;
+    }
+    return (n - nleft); /* return >= 0 */
+}
+
+
+/* Write "n" bytes to a descriptor */
+ssize_t writen(int fd, void *ptr, size_t n) {
+    size_t nleft;
+    ssize_t nwritten;
+
+    nleft = n;
+    while (nleft > 0) {
+        if ((nwritten = write(fd, ptr, nleft)) < 0) {
+            if (nleft == n) return -1; /* error, return -1 */
+            else break; /* error, return amount written so far */
+        } else if (nwritten == 0) break;
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+    return (n - nleft); /* return >= 0 */
+}
