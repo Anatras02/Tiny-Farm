2098d6f0f425461bab5f4f0f50f6f26ab9b27ea5
Anatras02 <alessio.balderi@adraise.it>
2022-06-28 16:15:19 +0200

    Update funzioni.h

--- funzioni/funzioni.h
+++ funzioni/funzioni.h
@@ -1,1 +1,10 @@
-char **get_nomi_file(int argc, char *argv[], int *n);
+char **get_nomi_file(int, char **, int *);
+
+int get_numero_cifre(long *);
+
+char *genera_stringa_socket(char *, long, int *);
+
+ssize_t readn(int, void *, size_t);
+
+ssize_t writen(int, void *, size_t);
+
